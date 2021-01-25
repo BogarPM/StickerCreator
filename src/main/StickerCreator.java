6d8a38f784248e456ee64828686682133abe111b
DRK970 <bogarperezmireles@gmail.com>
2021-01-25 12:10:50 -0600

    Created forms and this will be uploaded to github for the first time

---
+++ src/main/StickerCreator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author Bogar
+ */
+public class StickerCreator {
+    
+    
+    public static void main(String[] args){
+        MainFrame fr = new MainFrame();
+        fr.init(args);
+    }
+    
+    
+}
